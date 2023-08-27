2c4c83719b7e4730c42af8cbfa34e6326f99f4fc
sunnyineverywhere <sunnyinsummer@ewhain.net>
2023-08-28 01:46:38 +0900

    [FEAT] 질문 조회 시 id 반환 #3

--- q-api/src/main/java/com/qcard/api/question/dto/QuestionRes.java
+++ q-api/src/main/java/com/qcard/api/question/dto/QuestionRes.java
@@ -9,10 +9,13 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor
 public class QuestionRes {
+
+    private Long questionId;
     private String title;
     private String category;
 
     public QuestionRes(Question entity) {
+        this.questionId = entity.getId();
         this.title = entity.getTitle();
         this.category = entity.getCategory().name();
     }
