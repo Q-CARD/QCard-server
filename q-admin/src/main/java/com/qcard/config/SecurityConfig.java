d0fc37a969005b37772ed895d631dcbced180a73
마코레 <ashley_0726@naver.com>
2023-08-27 14:13:52 +0900

    [FEAT] H2사용을 위한 Security 설정

--- q-admin/src/main/java/com/qcard/config/SecurityConfig.java
+++ q-admin/src/main/java/com/qcard/config/SecurityConfig.java
@@ -1,7 +1,35 @@
 package com.qcard.config;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+import static org.springframework.security.web.util.matcher.AntPathRequestMatcher.antMatcher;
 
+@RequiredArgsConstructor
+@EnableWebSecurity
 @Configuration
 public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+                .cors(AbstractHttpConfigurer::disable)
+                .csrf(AbstractHttpConfigurer::disable)
+                .headers((headers) ->
+                        headers.frameOptions().sameOrigin() //iframe으로 화면을 구성한 페이지의 접근요청을 origin이 같을때만 허용
+                )
+                .authorizeHttpRequests((auth) ->
+                        auth.requestMatchers(antMatcher("/h2-dev/**")).permitAll()
+                )
+                .httpBasic(Customizer.withDefaults());
+
+        return httpSecurity.build();
+    }
+
 }
