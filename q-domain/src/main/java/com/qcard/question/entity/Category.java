a1ea46dae09fab6ef88de056ed12bfd08d3c1215
마코레 <ashley_0726@naver.com>
2023-08-27 18:07:08 +0900

    [FIX] Category Enum Name 수정
    related to #3

--- q-domain/src/main/java/com/qcard/question/entity/Category.java
+++ q-domain/src/main/java/com/qcard/question/entity/Category.java
@@ -4,18 +4,18 @@ import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 public enum Category {
-    CAT_NW("네트워크"),
-    CAT_OS("운영체제"),
-    CAT_DB("데이터베이스"),
-    CAT_DATA_STRUCTURE("자료구조"),
-    CAT_JAVA("JAVA"),
-    CAT_PYTHON("Python"),
-    CAT_JS("JavaScript"),
-    CAT_DEVOPS_INFRA("DevOps/Infra"),
-    CAT_BACK("Backend"),
-    CAT_FRONT("Frontend"),
-    CAT_AI("AI"),
-    CAT_인성("인성 질문");
+    CATEGORY_NW("네트워크"),
+    CATEGORY_OS("운영체제"),
+    CATEGORY_DB("데이터베이스"),
+    CATEGORY_DATA_STRUCTURE("자료구조"),
+    CATEGORY_JAVA("JAVA"),
+    CATEGORY_PYTHON("Python"),
+    CATEGORY_JS("JavaScript"),
+    CATEGORY_DEVOPS_INFRA("DevOps/Infra"),
+    CATEGORY_BACK("Backend"),
+    CATEGORY_FRONT("Frontend"),
+    CATEGORY_AI("AI"),
+    CATEGORY_인성("인성 질문");
 
 
     private final String label;
