205ba77b8b0f9a643ed23ad7649cfc604815167c
마코레 <ashley_0726@naver.com>
2023-08-27 17:52:27 +0900

    [FEAT] Category enum에 값 추가
    related to #3

--- q-domain/src/main/java/com/qcard/question/entity/Category.java
+++ q-domain/src/main/java/com/qcard/question/entity/Category.java
@@ -4,11 +4,19 @@ import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 public enum Category {
-    CAT_NW("CATEGORY_NETWORK", "네트워크"),
-    CAT_OS("CATEGORY_OPERATING_SYSTEM", "운영체제"),
-    CAT_DB("CATEGORY_DATABASE", "데이터베이스"),
-    CAT_DATA_STRUCTURE("CATEGORY_DATA_STRUCTURE", "자료구조");
+    CAT_NW("네트워크"),
+    CAT_OS("운영체제"),
+    CAT_DB("데이터베이스"),
+    CAT_DATA_STRUCTURE("자료구조"),
+    CAT_JAVA("JAVA"),
+    CAT_PYTHON("Python"),
+    CAT_JS("JavaScript"),
+    CAT_DEVOPS_INFRA("DevOps/Infra"),
+    CAT_BACK("Backend"),
+    CAT_FRONT("Frontend"),
+    CAT_AI("AI"),
+    CAT_인성("인성 질문");
 
-    private final String name;
-    private final String type;
+
+    private final String label;
 }
