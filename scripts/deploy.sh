1ac1a4816ad6bcb08514b5453861c3e9d45b56f2
sunnyineverywhere <sunnyinsummer@ewhain.net>
2023-08-28 18:15:33 +0900

    [CHORE] application.yml 변경

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,8 +1,8 @@
-REPOSITORY=/home/ubuntu/snapspot
+REPOSITORY=/home/ubuntu/qcard
 cd $REPOSITORY
 
 echo "> 도커 이미지 빌드"
 sudo docker build -t qcard-image .
 
 echo "> 도커 컨테이너 올리기"
-sudo docker run -d docker run -d --name qcard -p 80:8080 qcard-image
+sudo docker run -d --name qcard -p 80:8080 qcard-image
