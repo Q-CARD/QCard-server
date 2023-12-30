9455c3d9fb58400239533684bfc0e3b60112e6ed
sunnyineverywhere <sunnyinsummer@ewhain.net>
2023-12-31 03:07:46 +0900

    [FEAT] KafkaProducerService 작성

---
+++ q-infra/kafka/src/main/java/com/qcard/service/KafkaProducerService.java
@@ -0,0 +1,15 @@
+package com.qcard.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class KafkaProducerService {
+    private final KafkaTemplate<String, String> kafkaTemplate;
+
+    public void sendMessage(String topic, String message) {
+        kafkaTemplate.send(topic, message);
+    }
+}
